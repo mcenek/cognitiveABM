dcb07447cfbccd4437d41fda5c241ba5c86b494a
Matttran7 <matttran385701@gmail.com>
2023-08-06 23:50:17 -0700

    revised getNewExportVals function for clarity

--- CognitiveABM/agentInfoHolder.cs
+++ CognitiveABM/agentInfoHolder.cs
@@ -50,22 +50,23 @@ namespace CognitiveABM.agentInformationHolder
        */
       public List<float[]> getNewExportVals(int id, List<float[]> exportVals){
         //agent info is our patchdict
-        List<float[]> tempList = agentInfo[id].Item1;
-        float[] temp = exportVals.Last();
-        temp[12] = (tempList.Last())[11];
-        // calculate the altitude by getting the differences 
-        temp[13] = temp[11] - temp[12];
+        List<float[]> infoList = agentInfo[id].Item1;
+        float[] holdInfo = exportVals.Last();
+        holdInfo[12] = (infoList.Last())[11];
+        /* 
+          calculate the altitude by getting the differences 
+          from the path that the agent took: temp[11] (initial val) temp[12] compared value
+          stored into temp[13] which is the difference between the two altitudes
+        */
+        holdInfo[13] = holdInfo[11] - holdInfo[12];
         // hill descending -> flips the values (3 5 7 8 4 4 1 -> differences -> 2 2 2 1 -4 0 -3)
-        temp[13] = -1*temp[13];
-        Console.WriteLine("Temp11: "+ temp[11]);
-        Console.WriteLine("Temp12: "+ temp[12]);
-        Console.WriteLine("Temp13: "+ temp[13]);
-        float[] avgMax = getAverageandTotal(tempList, temp[13]);
-        temp[14] = avgMax[0];
-        temp[15] = avgMax[1];
+        holdInfo[13] = -1*holdInfo[13];
+        float[] avgMax = getAverageandTotal(infoList, holdInfo[13]);
+        holdInfo[14] = avgMax[0];
+        holdInfo[15] = avgMax[1];
         //fitness.Add(temp[13]);
-        tempList.Add(temp);
-        return tempList;
+        infoList.Add(holdInfo);
+        return infoList;
       }
 
 
